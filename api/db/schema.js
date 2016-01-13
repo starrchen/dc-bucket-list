04c1729e8ca21b7182448b1e1091bdde57692f5a
Starr Z. Chen <chenstarr@gmail.com>
2016-01-13 18:48:35 -0500

    Tweaking UX, new button filters

--- api/db/schema.js
+++ api/db/schema.js
@@ -3,11 +3,14 @@ var Schema = mongoose.Schema,
     ObjectId = Schema.ObjectId;
 
 var ItemSchema = new Schema({
+  // ofFifty: Number,
   name: String,
   description: String,
   website: String,
   free: Boolean,
-  date: String
+  date: String,
+  planAhead: Boolean,
+  metro: Boolean
 });
 
 var ItemModel = mongoose.model("Item", ItemSchema);
