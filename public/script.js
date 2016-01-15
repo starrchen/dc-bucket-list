1d2ecebb9d427fa85b23c4e50d169989d678a75f
Starr Z. Chen <chenstarr@gmail.com>
2016-01-15 06:57:56 -0500

    Reinstating Bootstrap CSS

--- public/script.js
+++ public/script.js
@@ -1,10 +1,1 @@
 console.log("YUP");
-
-var app = angular.module('gwbl', []);
-
-app.controller('DoneController', function($scope) {
-  $scope.clearFilter = function() {
-    console.log("CLEAR");
-    $scope.query = {};
-  };
-});
