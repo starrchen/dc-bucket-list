875bc955b8c9f7151d8416d84d345ac953bcf74a
Starr Z. Chen <chenstarr@gmail.com>
2016-01-15 01:52:10 -0500

    added multifilters

--- public/script.js
+++ public/script.js
@@ -1,1 +1,10 @@
 console.log("YUP");
+
+var app = angular.module('gwbl', []);
+
+app.controller('DoneController', function($scope) {
+  $scope.clearFilter = function() {
+    console.log("CLEAR");
+    $scope.query = {};
+  };
+});
